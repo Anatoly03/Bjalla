5dac393056b352c5ef575853110181810cb04de0
Anatoly Wein <anatoly.wein@gmail.com>
2026-02-15 20:04:10 +0100

    add more user tests

--- bjalla-api/tests/api_users_test.go
+++ bjalla-api/tests/api_users_test.go
@@ -1,9 +1,12 @@
-package api_test
-
-// TEST SOURCE FROM
+// API Testing for Users Collection
+//
+// https://pocketbase.io/docs/go-testing/
 // https://github.com/presentator/presentator/blob/master/api_users_test.go
 
+package api_test
+
 import (
+	"encoding/json"
 	"net/http"
 	"path/filepath"
 	"runtime"
@@ -13,6 +16,7 @@ import (
 	"github.com/pocketbase/pocketbase/tests"
 )
 
+// setupTestApp initializes a new TestApp instance.
 func setupTestApp(t testing.TB) *tests.TestApp {
 	_, filePath, _, _ := runtime.Caller(0)
 	dataDir := filepath.Clean(filepath.Join(filepath.Dir(filePath), "..", "pb_data"))
@@ -26,7 +30,22 @@ func setupTestApp(t testing.TB) *tests.TestApp {
 	return testApp
 }
 
-// TestUsersCreate ensures everyone can create a new user record
+// getAuthToken is a helper function to retrieve an auth token for a given user in the specified collection.
+func getAuthToken(app *tests.TestApp, recordId string) string {
+	record, err := app.FindRecordById("users", recordId)
+	if err != nil {
+		app.Logger().Error("Failed to find auth record with id %q: %v", recordId, err)
+	}
+
+	token, err := record.NewAuthToken()
+	if err != nil {
+		app.Logger().Error("Failed to generate token for auth record with id %q: %v", recordId, err)
+	}
+
+	return token
+}
+
+// TestUsersCreate ensures everyone can create a new unique user record
 func TestUsersCreate(t *testing.T) {
 	t.Parallel()
 
@@ -36,15 +55,15 @@ func TestUsersCreate(t *testing.T) {
 			Method: http.MethodPost,
 			URL:    "/api/collections/users/records",
 			Body: strings.NewReader(`{
-				"name":            "test_new",
-				"email":           "test_new@example.com",
+				"name":            "test",
+				"email":           "test@example.com",
 				"password":        "1234567890",
 				"passwordConfirm": "1234567890"
 			}`),
 			TestAppFactory: setupTestApp,
 			ExpectedStatus: 200,
 			ExpectedContent: []string{
-				`"name":"test_new"`,
+				`"name":"test"`,
 				`"verified":false`,
 			},
 			ExpectedEvents: map[string]int{
@@ -61,6 +80,115 @@ func TestUsersCreate(t *testing.T) {
 				"OnRecordAfterCreateSuccess": 1,
 			},
 		},
+		{
+			Name:   "creating user with existing email should fail",
+			Method: http.MethodPost,
+			URL:    "/api/collections/users/records",
+			Body: strings.NewReader(`{
+				"name":            "test_new",
+				"email":           "test@example.com",
+				"password":        "1234567890",
+				"passwordConfirm": "1234567890"
+			}`),
+			ExpectedStatus: 400,
+			ExpectedContent: []string{
+				`"message":"Value must be unique."`,
+			},
+			ExpectedEvents: map[string]int{
+				"*":                        0,
+				"OnModelAfterCreateError":  1,
+				"OnModelCreate":            1,
+				"OnModelCreateExecute":     1,
+				"OnModelValidate":          1,
+				"OnRecordAfterCreateError": 1,
+				"OnRecordCreate":           1,
+				"OnRecordCreateExecute":    1,
+				"OnRecordCreateRequest":    1,
+				"OnRecordValidate":         1,
+			},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		scenario.Test(t)
+	}
+}
+
+// Ensure compliance with GDPR right to erasure: https://gdpr-info.eu/art-17-gdpr/
+// https://eur-lex.europa.eu/eli/reg/2023/2854/oj/eng
+func TestUsersDelete(t *testing.T) {
+	t.Parallel()
+
+	userId := "null"
+	// authToken := "null"
+
+	scenarios := []tests.ApiScenario{
+		{
+			Name:                  "guests trying to delete non-existant users should not break",
+			Method:                http.MethodDelete,
+			URL:                   "/api/collections/users/records/pb_record_users",
+			TestAppFactory:        setupTestApp,
+			DisableTestAppCleanup: true,
+			ExpectedStatus:        404,
+			ExpectedContent:       []string{`"data":{}`},
+			ExpectedEvents:        map[string]int{"*": 0},
+		},
+		{
+			Method: http.MethodPost,
+			URL:    "/api/collections/users/records",
+			Body: strings.NewReader(`{
+				"name":            "test",
+				"email":           "test@example.com",
+				"password":        "1234567890",
+				"passwordConfirm": "1234567890"
+			}`),
+			TestAppFactory:        setupTestApp,
+			DisableTestAppCleanup: true,
+			AfterTestFunc: func(t testing.TB, app *tests.TestApp, res *http.Response) {
+				var responseData struct {
+					Id string `json:"id"`
+				}
+
+				decoder := json.NewDecoder(res.Body)
+				if err := decoder.Decode(&responseData); err != nil {
+					t.Fatalf("Failed to decode create user response: %v", err)
+				}
+
+				userId = responseData.Id
+				// authToken = getAuthToken(app, userId)
+			},
+			ExpectedStatus: 200,
+			ExpectedContent: []string{
+				`"name":"test"`,
+			},
+		},
+		{
+			Name:                  "guests should not be able to delete existing users",
+			Method:                http.MethodDelete,
+			URL:                   "/api/collections/users/records/" + userId,
+			DisableTestAppCleanup: true,
+			ExpectedStatus:        404,
+			ExpectedContent:       []string{`"data":{}`},
+			ExpectedEvents:        map[string]int{"*": 0},
+		},
+		// TODO: enable once brave to fix
+		// {
+		// 	Name:   "every user should be allowed to delete themselves",
+		// 	Method: http.MethodDelete,
+		// 	URL:    "/api/collections/users/records/" + userId,
+		// 	Headers: map[string]string{
+		// 		"Authorization": authToken,
+		// 	},
+		// 	DisableTestAppCleanup: true,
+		// 	ExpectedStatus:        204,
+		// 	ExpectedContent: []string{
+		// 		`null`,
+		// 	},
+		// 	ExpectedEvents: map[string]int{
+		// 		"*":                     0,
+		// 		"OnRecordDeleteRequest": 1,
+		// 	},
+		// },
 	}
 
 	for _, scenario := range scenarios {
